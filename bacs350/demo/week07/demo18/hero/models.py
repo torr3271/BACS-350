5602d8c334f7e324170281970c2f88d6ca619098
torr3271 <60019533+torr3271@users.noreply.github.com>
2020-10-05 15:17:50 -0600

    update

--- bacs350/demo/week07/demo18/hero/models.py
+++ bacs350/demo/week07/demo18/hero/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Superhero(models.Model):
     name = models.CharField(max_length=20)
     identity = models.CharField(max_length=20)
+    image = models.CharField(max_length=20)
     
     def __str__(self):
         return self.name
--- bacs350/demo/week07/demo18/hero/views.py
+++ bacs350/demo/week07/demo18/hero/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
+from .models import Superhero
 
-# Create your views here.
+class HeroView(TemplateView):
+    template_name="hero.html"
+    
+    def get_context_data(self, **kwargs):
+        heroes = Superhero.objects.all()
+        return{
+            'title':'Superhero  Profile',
+            'heroes': heroes,
+        }
