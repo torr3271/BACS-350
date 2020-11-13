66965d61431342cb9852d92211d9f13d4020300e
torr3271 <60019533+torr3271@users.noreply.github.com>
2020-11-13 14:22:15 -0700

    fiddling with settings

--- bacs350/Superhero/Superhero/urls.py
+++ bacs350/Superhero/Superhero/urls.py
@@ -1,6 +1,4 @@
 """Superheros URL Configuration
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
